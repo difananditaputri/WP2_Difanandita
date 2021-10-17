62bd9c8aebad0891f931fc5f3a34453c8e0a5b23
difananditaputri <dnanditap@gmail.com>
2021-10-17 16:40:57 +0700

    latihan web pertemuan3

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Difa</li>
+        <li>Nama Belakang : Nandita Putri</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+         <li> Jalan pedongkelan brlakang</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+         <li>Bulutangkis</li>
+         <li>sepak bola</li>
+    </ul>
+</section>
